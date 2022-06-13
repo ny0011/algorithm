ec67d8601e2db93a9b5b8e6e8046e266fb552e4a
ny0011 <nayun011@gmail.com>
2022-06-13 14:49:09 +0900

    if문을 줄이고 자료구조를 이차원 배열로 변경

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 solve the problem👿
 
+## Leetcode
+
 884. Uncommon Words from Two Sentences
-     https://leetcode.com/problems/uncommon-words-from-two-sentences/submissions/
+
+- https://leetcode.com/problems/uncommon-words-from-two-sentences/submissions/
+
+## Programmers
+
+60057. 문자열 압축
+
+- https://programmers.co.kr/learn/courses/30/lessons/60057
--- programmers/60057.py
+++ programmers/60057.py
@@ -1,37 +1,29 @@
-# 문자열 압축
-# https://programmers.co.kr/learn/courses/30/lessons/60057
 # 굿 테스트케이스 : solution("aaaaaaaaaaaabcd");  // 6
 
 def CompressNumCharacter(s, n):
     c = s[:n]
-    check = [c]
-    count = 1
+    new_s = [[c,1]]
+    new_s_index = 0
+    new_s_count = 1
     s_length = len(s)
     for i in range(n,s_length,n):
         value = s[i:i+n]
         if i+n > s_length:
             value = s[i:s_length]
+            
         if c == value:
-            count += 1              
-        else:            
-            if count != 1:
-                check.append(str(count))
+            new_s[new_s_index][new_s_count] += 1              
+        else:
             c = value
-            count = 1
-            check.append(value)
-    if count != 1:
-        check.append(str(count))
-    check_str = ''.join(check)
-    if len(check_str) != len(s):
-        return len(check_str)
-    return len(s)
+            new_s_index += 1 
+            new_s.append([c,1])
+    
+    check_str = str()
+    for arr in new_s:
+        for v in arr:
+            if v != 1:
+                check_str += str(v)
+    return len(check_str)
 
 def solution(s):
-    answer = len(s)
-    mid = len(s)//2 + 1
-    for i in range(1, mid):
-        ret = CompressNumCharacter(s,i)
-        if answer > ret:
-            answer = ret
-    
-    return answer
+    return min(CompressNumCharacter(s,i) for i in range(1, len(s)//2 + 2 ))
