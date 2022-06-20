c98d6712e0c7182432c26375ad528e91d192127d
ny0011 <nayun011@gmail.com>
2022-06-20 18:22:37 +0900

    최대값만 비교할 때는 굳이 중간 과정을 저장할 필요가 없음

--- programmers/42586.py
+++ programmers/42586.py
@@ -1,20 +1,15 @@
 def populate_date(progress, speed):
     q, r = divmod(100 - progress, speed) 
-    return q + bool(r)
+    return q + int(bool(r))
 
 def solution(progresses, speeds):
-    dates = []
+    answer = []
+    max = 0
     for i in range(len(speeds)):
-        dates.append(populate_date(progresses[i], speeds[i]))
-    top = dates.pop(0)
-    answer = [1]
-    i_answer = 0
-    while len(dates) > 0:
-        if top < dates[0]:            
-            top = dates.pop(0)
+        top = populate_date(progresses[i], speeds[i])
+        if len(answer) == 0 or max < top:
+            max = populate_date(progresses[i], speeds[i])
             answer.append(1)
-            i_answer +=1
         else:
-            answer[i_answer] +=1
-            dates.pop(0)
+            answer[-1]+=1
     return answer
