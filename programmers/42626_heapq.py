a2eb489ab2132bb5139b49dd622cb5f7828e240e
ny0011 <nayun011@gmail.com>
2022-06-20 15:25:07 +0900

    add description

--- programmers/42626_heapq.py
+++ programmers/42626_heapq.py
@@ -14,6 +14,12 @@ def solution(scoville, K):
     return answer
 
 '''
+heapq(min heap)를 사용하면 가장 앞에 있는 값이 항상 제일 작은 값이 됨.
+heapq에서 두번 pop을 한 후 새로 섞은 값을 push 하면 됨.
+pop : O(logn)
+push : O(logn)
+가 되어 총 시간복잡도는 O(nlogn) 
+
 효율성  테스트
 테스트 1 〉	통과 (169.62ms, 16MB)
 테스트 2 〉	통과 (401.35ms, 21.9MB)
