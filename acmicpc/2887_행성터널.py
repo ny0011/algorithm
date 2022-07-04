a4cf95a05f5248aee6330673a83af966e51500b3
ny0011 <nayun011@gmail.com>
2022-07-04 14:19:58 +0900

    try it

--- acmicpc/2887_행성터널.py
+++ acmicpc/2887_행성터널.py
@@ -1,6 +1,14 @@
+
+
 MAX = 2000000001
 def calculate_cost(p1, p2):
     return min(abs(p1[0]-p2[0]),abs(p1[1]-p2[1]),abs(p1[2]-p2[2]))
+
+def push_item(graph, start, end):
+    if start not in graph:
+        graph[start] = set([end])
+    else:
+        graph[start] = end
 
 def main():
     n = int(input())
@@ -9,16 +17,20 @@ def main():
         arr.append(list(map(int, input().split(" "))))
 
     answer = 0
-    connected = [0] * (n)
+    graph = dict()
+    count = 0
     for i in range(n):
         _min = MAX
         for j in range(n):
+            count+=1
             if i != j:
                 cost = calculate_cost(arr[i], arr[j])
-                if cost < _min:
-                    connected[i] = j
-                    _min = cost
-        print(_min, connected)
+                if cost <= _min:
+                    graph[i] = j
+                    _min = cost                
+                if cost == 0:
+                    break
+        print(_min, graph)
         answer += _min
-    print(answer)
+    print(answer, count)
 main()
